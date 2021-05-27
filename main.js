11b1a43af74b67c878a5499cf9ac1a5ad4ec00dd
Aleksi Rissa <54816696+Kataya42@users.noreply.github.com>
2021-05-27 20:50:11 +0300

    Removed Pin message deletion, added new "hush" command

--- main.js
+++ main.js
@@ -12,15 +12,16 @@ client.once("ready", () =>{
 });
 
 
+
 client.on('messageReactionAdd', (reaction, user) => {
     let message = reaction.message, emoji = reaction.emoji;
 
-    if (emoji.name == '✅') {
+    if (emoji.name == '😋') {
         message.channel.send("yes");
         reaction.remove(user);
     }
 
-    else if (emoji.name == '📌') {
+    else if (emoji.name == 'pushpin') {
         
         if (message.pinned){
             message.unpin()
@@ -28,68 +29,77 @@ client.on('messageReactionAdd', (reaction, user) => {
             message.pin()
         }
         reaction.remove(user);
+        console.log("Pin added by:", user)
+
     } else {
 
     }
 
-    
 });
 
 client.on("message",message=>{
-    if (message.type === "PINS_ADD") {
-        message.delete(); 
-     } else {
+
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
     
-        if(!message.content.startsWith(prefix) || message.author.bot) return;
+    const command = args.shift().toLocaleLowerCase();
+    const input = args.slice(-1)[0];
+    
+    if( command === "ping"){
 
-        const args = message.content.slice(prefix.length).split(/ +/);
+        message.channel.send("pong");
+    
+    } else if( command === "quote" && args.length == 1){
+
+        fs.readFile('quotes.json', 'utf-8', (err, data) => {
+            if (err) {
+                throw err;
+            } 
+            var suola = JSON.parse(data);
+            var name = input.toLocaleLowerCase();
+
+            if (suola["people"].hasOwnProperty(name)){
+
+                //list of quotes by people
+                const quoteList = suola["people"][name]["quotes"];
+                const random = Math.floor(Math.random() * quoteList.length);
+
+                // should the bot add a signature to the end of the quote or not
+                if(suola["people"][name]["signature"] == true){
+                    message.channel.send('"'+quoteList[random]+'"' + mark + suola["people"][name]["name"])
+                } else{
+                    message.channel.send(quoteList[random])
+                }
+                
+
+            } 
+        });
+                
         
-        const command = args.shift().toLocaleLowerCase();
-        const input = args.slice(-1)[0];
-        
-        if( command === "ping"){
+    } else if(command === "help"){
+        message.channel.send("List of commands: help, quote, hush.\nYou can also react to a message with :pushpin: to pin a message");
 
-            message.channel.send("pong");
-        
-        } else if( command === "quote" && args.length == 1){
+    } else if(command === "raidtime"){
+        //TO BE IMPLEMENTED
+        message.channel.send("");
 
-            fs.readFile('quotes.json', 'utf-8', (err, data) => {
-                if (err) {
-                    throw err;
-                } 
-                var suola = JSON.parse(data);
-                var name = input.toLocaleLowerCase();
+    } else if(command === "hush"){
 
-                if (suola["people"].hasOwnProperty(name)){
+            picList = ["./hush2.png", "./hush3.png", "./hush4.png"]
+            const random = Math.floor(Math.random() * picList.length);
 
-                    //list of quotes by people
-                    const quoteList = suola["people"][name]["quotes"];
-                    const random = Math.floor(Math.random() * quoteList.length);
-
-                    // should the bot add a signature to the end of the quote or not
-                    if(suola["people"][name]["signature"] == true){
-                        message.channel.send('"'+quoteList[random]+'"' + mark + suola["people"][name]["name"])
-                    } else{
-                        message.channel.send(quoteList[random])
-                    }
-                    
-
-                } 
+            message.channel.send('', {
+                files: [
+                    picList[random]
+                ]
             });
-                    
-            
-        } else if(command === "help"){
-            message.channel.send("!quote + name to give quote \n\nSide note: quotettavat henkilöt ovat: \nMiika, Olli, Leia, Jaska, Miranda, Anri, Jaakko, Konsta, Juhani, Aleksi, Luca, Toni, Fisu, suola");
 
+    } else{
+        //message.channel.send("bork");
         
-        } else if(command === "raidtime"){
-            message.channel.send("");
-
-        } else{
-            //message.channel.send("bork");
-            
-        }
     }
+    
 });
 
 fs.readFile('token.txt', 'utf-8', (err, data) => {
