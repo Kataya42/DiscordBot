ff0cb23bf96ca1fa254365ad35f841bfb42295db
Aleksi Rissa <aleksi.rissa@gmail.com>
2022-06-03 01:04:40 +0300

    Added new quotes, removed unused functions and fixed interaction with voice text channels

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 const mark = " - ";
 
 // prefix to call the bot
@@ -11,31 +11,41 @@ client.once("ready", () =>{
     console.log(" bot is online :) ");
 });
 
-
-
-
+/*
 client.on("presenceUpdate", function(oldMember, newMember){
+    
     if(newMember.user.username = "Kataya"){ // Kataya
         if (newMember.guild.id == "788773416822898738"){ //Bot test
             if(newMember.userID == "153854949762138113"){ // Kataya
                 if(newMember.activities.length != 0){
-                    if (newMember.activities[0].applicationID == "572456126872944651"){ // Satisfactory
-                        channel = client.channels.cache.get('884501406546026536'); // Tehdasmemes factory-status channel
-                        channel.send('Factory is open :)');
-
+                    //console.log(newMember.activities)
+                    for (var act in newMember.activities){
+                        //console.log( newMember.activities[act]);
+                        if (newMember.activities[act].applicationID == "572456126872944651"){ // Satisfactory
+                            channel = client.channels.cache.get('884501406546026536'); // Tehdasmemes factory-status channel
+                            channel.send('Factory is open :)');
+    
+                        }
                     }
-                } else if(oldMember.activities.length !=0){
-                    if (oldMember.activities[0].applicationID == "572456126872944651"){ // Satisfactory
-                        channel = client.channels.cache.get('884501406546026536'); //Tehdasmemes factory-status channel
-                        channel.send('Factory is closed :(');
+                    //console.log(newMember.activities)
+                    
+                }
+                
+                if(oldMember.activities.length !=0){
+
+                    for (var act2 in oldMember.activities){
+                        //console.log( oldMember.activities[act]);
+                        if (oldMember.activities[act2].applicationID == "572456126872944651"){ // Satisfactory
+                            channel = client.channels.cache.get('884501406546026536'); //Tehdasmemes factory-status channel
+                            channel.send('Factory is closed :(');
+                        }
                     }
                 }    
             } 
         }
     }
 });
-
-
+*/
 
 client.on('messageReactionAdd', (reaction, user) => {
     let message = reaction.message, emoji = reaction.emoji;
@@ -61,7 +71,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 
 });
 
-client.on("message",message=>{
+client.on("messageCreate",message=>{
 
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
@@ -71,9 +81,9 @@ client.on("message",message=>{
     const input = args.slice(-1)[0];
     
     if( command === "ping"){
-
+        /*
         message.channel.send("pong");
-    
+        */
     } else if( command === "quote" && args.length == 1){
 
         fs.readFile('staticQuotes.json', 'utf-8', (err, content) => {
@@ -85,7 +95,7 @@ client.on("message",message=>{
             if (err) {
                 throw err;
             } 
-
+                
                 if (message.guild.id == "588550599230881793"){
                     var suola = JSON.parse(content);
                 } else {
@@ -106,24 +116,25 @@ client.on("message",message=>{
                     }
                     
                 }
+                
         });
     });
                 
     } else if(command === "help"){
         message.channel.send("List of commands: help, quote, hush, ping, random.\nYou can also react to a message with :pushpin: to pin a message");
-
+    /*  NOT IMPLEMENTED  
     } else if(command === "raidtime"){
 
-        //channel = client.channels.cache.get('788773416822898741');
-        //channel.send('Pong');
-        //message.channel.send("a");
-
+        channel = client.channels.cache.get('788773416822898741');
+        channel.send('Pong');
+        message.channel.send("a");
+    
     } else if(command === "yuuki"){
         message.channel.send("<@114403054505295880> DONT ASSIZE");
 
     } else if(command === "hentai"){
         message.channel.send("sus");
-
+    */
     } else if(command === "random") {
      
         fs.readFile('quotes.json', 'utf-8', (err, data) => {
@@ -168,7 +179,7 @@ client.on("message",message=>{
                     './pics/' + chosenFile    
                 ]
             });
-
+    
     } else{
         //message.channel.send("bork");
     
